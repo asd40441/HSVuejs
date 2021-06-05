4a49534a636b28205cc1b6d0cd488e8daf74369f
asd40441 <asd40441@hotmail.com>
2021-06-05 17:42:24 +0800

    updata

--- SecondWeek/login.js
+++ SecondWeek/login.js
@@ -4,39 +4,40 @@ const password = document.querySelector('#password');
 const btn = document.querySelector('#btn');
 
 const app = {
-    url: 'https://vue3-course-api.hexschool.io',    // 站點
-    path: 'asd40441',   // api path
+    url: 'https://vue3-course-api.hexschool.io', // 站點
+    path: 'asd40441', // api path
     user: {
-        "username": '',
-        "password": ''
+        username: '',
+        password: ''
     },
     loginUser() {
-        btn.addEventListener('click', e => {    // 點擊登入
+        btn.addEventListener('click', e => { // 點擊登入
             this.user.username = email.value;
             this.user.password = password.value;
-            axios.post(`${this.url}/admin/signin`, this.user)    // 傳送登入請求
+            axios.post(`${this.url}/admin/signin`, this.user) // 傳送登入請求
                 .then(res => {
                     console.log(res);
-                    if(res.data.success){
+                    if (res.data.success) {
                         console.log("登入成功");
                         alert("登入成功");
-                    }else{
+                    } else {
                         console.log("登入失敗");
                         alert("登入失敗");
                         return;
                     }
-                    const token = res.data.token;
-                    const expired = res.data.expired;
-                    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`;    // 儲存cookie
+                    // const token = res.data.token;
+                    // const expired = res.data.expired;
+                    const [token, expired] = [res.data.token, res.data.expired];
+                    document.cookie = `hexToken=${token}; expires=${new Date(expired)}`; // 儲存cookie
                     window.location.href = "admin.html";
                 })
-                .catch(res=>{
+                .catch(res => {
                     console.log(res);
                 })
         });
 
     },
-    init(){
+    init() {
         this.loginUser();
     }
 }
