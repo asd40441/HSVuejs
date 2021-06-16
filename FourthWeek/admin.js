b1956684ace0dffe6d18daa22724759d09d8712b
asd40441 <asd40441@hotmail.com>
2021-06-16 23:15:52 +0800

    updata

--- FourthWeek/admin.js
+++ FourthWeek/admin.js
@@ -1,3 +1,7 @@
+import pagination from "./pagination.js";
+let productModal = null;
+let delProductModal = null;
+
 const app = Vue.createApp({
     data() {
         return {
@@ -12,6 +16,9 @@ const app = Vue.createApp({
             pagination: {}
         }
     },
+    components: {   // 區域註冊
+        pagination,
+    },
     mounted() { // 生命週期
         productModal = new bootstrap.Modal(document.getElementById('productModal')); // 取得Modal元素
         delProductModal = new bootstrap.Modal(document.getElementById('delProductModal')); // 取得delModal元素
@@ -22,7 +29,7 @@ const app = Vue.createApp({
         this.getProducts();
     },
     methods: {
-        getProducts(page) { // 取得產品列表
+        getProducts(page = 1) { // 取得產品列表
             this.loading = true;
             let apiUrl = `${this.url}/api/${this.path}/admin/products?page=${page}`;
             axios.get(apiUrl)
@@ -42,15 +49,15 @@ const app = Vue.createApp({
                     console.log(res);
                 })
         },
-        productUpdata() { // 新增產品或修改
+        productUpdata(tempProduct) { // 新增產品或修改
             let apiUrl = `${this.url}/api/${this.path}/admin/product`;
             let http = 'post';
             if (!this.newData) {
-                apiUrl = `${this.url}/api/${this.path}/admin/product/${this.tempProduct.id}`;
+                apiUrl = `${this.url}/api/${this.path}/admin/product/${tempProduct.id}`;
                 http = 'put';
             }
             axios[http](apiUrl, {
-                    data: this.tempProduct
+                    data: tempProduct
                 })
                 .then(res => {
                     console.log('成功', res);
@@ -93,39 +100,130 @@ const app = Vue.createApp({
                     console.log(res);
                 })
         },
+    },
+});
+
+app.component('productModal',{
+    template: `
+    <div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog modal-xl">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-dark text-white">
+            <h5 id="productModalLabel" class="modal-title">
+              <span>新增產品</span>
+            </h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="row">
+
+              <div class="col-sm-4">
+                <div class="form-group">
+                  <label for="imageUrl">主要圖片</label>
+                  <input type="text" class="form-control" placeholder="請輸入圖片連結" v-model="tempProduct.imageUrl">
+                  <img class="img-fluid" :src="tempProduct.imageUrl">
+                </div>
+                <div class="mb-1">多圖新增</div>
+                <div v-if="Array.isArray(tempProduct.imagesUrl)">
+                  <div class="mb-1" v-for="(image, key) in tempProduct.imagesUrl" :key="key">
+                    <div class="form-group">
+                      <label for="imageUrl">圖片網址</label>
+                      <input type="text" class="form-control" placeholder="請輸入圖片連結"
+                        v-model="tempProduct.imagesUrl[key]">
+                      <button class="btn btn-outline-danger btn-sm d-block w-30" @click="tempProduct.imagesUrl.splice(key,1)">
+                        刪除
+                      </button>
+                    </div>
+                    <img class="img-fluid" :src="image">
+                  </div>
+                  <div v-if="!tempProduct.imagesUrl.length || tempProduct.imagesUrl[tempProduct.imagesUrl.length - 1] && tempProduct.imagesUrl.length < 5">
+                    <button class="btn btn-outline-primary btn-sm d-block w-100"
+                      @click="tempProduct.imagesUrl.push('')">
+                      新增圖片
+                    </button>
+                  </div>
+                </div>
+                <div v-else>
+                  <button class="btn btn-outline-primary btn-sm d-block w-100" @click="createImagesArray">
+                    新增陣列圖片
+                  </button>
+                </div>
+              </div>
+
+              <div class="col-sm-8">
+                <div class="form-group">
+                  <label for="title">標題</label>
+                  <input id="title" type="text" class="form-control" placeholder="請輸入標題" v-model="tempProduct.title">
+                </div>
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="category">分類</label>
+                    <input id="category" type="text" class="form-control" placeholder="請輸入分類"
+                      v-model="tempProduct.category">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="price">單位</label>
+                    <input id="unit" type="text" class="form-control" placeholder="請輸入單位" v-model="tempProduct.unit">
+                  </div>
+                </div>
+
+                <div class="row">
+                  <div class="form-group col-md-6">
+                    <label for="origin_price">原價</label>
+                    <input id="origin_price" type="number" min="0" class="form-control" placeholder="請輸入原價"
+                      v-model.number="tempProduct.origin_price">
+                  </div>
+                  <div class="form-group col-md-6">
+                    <label for="price">售價</label>
+                    <input id="price" type="number" min="0" class="form-control" placeholder="請輸入售價"
+                      v-model.number="tempProduct.price">
+                  </div>
+                </div>
+                <hr>
+
+                <div class="form-group">
+                  <label for="description">產品描述</label>
+                  <textarea id="description" type="text" class="form-control" placeholder="請輸入產品描述"
+                    v-model="tempProduct.description">
+                    </textarea>
+                </div>
+                <div class="form-group">
+                  <label for="content">說明內容</label>
+                  <textarea id="description" type="text" class="form-control" placeholder="請輸入說明內容"
+                    v-model="tempProduct.content">
+                    </textarea>
+                </div>
+                <div class="form-group">
+                  <div class="form-check">
+                    <input id="is_enabled" class="form-check-input" type="checkbox" :true-value="1" :false-value="0"
+                      v-model="tempProduct.is_enabled">
+                    <label class="form-check-label" for="is_enabled">是否啟用</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+              取消
+            </button>
+            <button type="button" class="btn btn-primary" @click="$emit('updata-product' , tempProduct)">
+              確認
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    `,
+    props: ['tempProduct'],
+    methods: {
         createImagesArray() {   // 建立圖片陣列
             this.tempProduct.imagesUrl = [
                 ''
             ];
-        }
-    },
-});
-
-app.component('pagination', {
-    props: ['page'],
-    template: `
-    <ul class="pagination">
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Previous">
-        <span aria-hidden="true">&laquo;</span>
-      </a>
-    </li>   
-    <li class="page-item" :class="{ 'active': item === page.current_page }" v-for="item in page.total_pages" :key="item">
-        <a class="page-link" href="#" @click="$emit('get-products' , item)">{{ item }}</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#" aria-label="Next">
-        <span aria-hidden="true">&raquo;</span>
-      </a>
-    </li>
-  </ul>
-    `,
-    data() {
-        return {}
-    },
-    created() {
-        // console.log(this.page);
+        },
     }
 })
-
 
 app.mount("#app");
