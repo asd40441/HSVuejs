32077fc2f89d2776db85eab86471d7b6a4d6540a
asd40441 <asd40441@hotmail.com>
2021-06-23 23:07:23 +0800

    updata

--- FifthWeek/all.js
+++ FifthWeek/all.js
@@ -3,8 +3,6 @@ import productModal from './productModal.js'
 const url = 'https://vue3-course-api.hexschool.io'; // 站點
 const path = 'asd40441'; // api path
 
-
-
 const app = Vue.createApp(
     {
         data() {
@@ -37,26 +35,30 @@ const app = Vue.createApp(
             axios.get(api)
             .then(res=>{
                 this.products = res.data.products;
-                console.log(res);
+                console.log('取得商品列表',res);
+                this.loadingStatus.loadingItem = '';
             })
             .catch(err=>{
                 console.log(err);
             })
         },
         openModal(item){
+            this.loadingStatus.loadingItem = item.id;
             const api = `${url}/api/${path}/product/${item.id}`;
             axios.get(api)
             .then(res=>{
-                console.log(res);
+                console.log('打開Modal',res);
                 this.product = res.data.product;
-                console.log(this.product);
+                // console.log(this.product);
                 this.$refs.userProductModal.openModal();
+                this.loadingStatus.loadingItem = '';
             })
             .catch(err=>{
                 console.log(err);
             })
         },
         addCart(id , qty = 1){
+            this.loadingStatus.loadingItem = id;
             const api = `${url}/api/${path}/cart`;
             const cart = {
                 "product_id" : id,
@@ -64,7 +66,8 @@ const app = Vue.createApp(
             };
             axios.post(api , {data : cart})
             .then(res=>{
-                console.log(res);
+                console.log('新增購物車',res);
+                this.getCart();
             })
             .catch(err=>{
                 console.log(err);
@@ -74,9 +77,50 @@ const app = Vue.createApp(
             const api = `${url}/api/${path}/cart`;
             axios.get(api)
             .then(res=>{
-                // console.log(res);
                 this.cart = res.data.data;
-                console.log(this.cart);
+                console.log('更新購物車',res);
+                this.loadingStatus.loadingItem = '';
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        },
+        updataCart(item){
+            this.loadingStatus.loadingItem = item.id;
+            const api = `${url}/api/${path}/cart/${item.id}`;
+            const cart = {
+                product_id : item.product.id,
+                qty : item.qty
+            }
+            // console.log(cart);
+            axios.put(api,{data : cart})
+            .then(res=>{
+                console.log('修改購物車',res);
+                this.getCart();
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        },
+        clearCart(){
+            this.loadingStatus.loadingItem = 'clear';
+            const api = `${url}/api/${path}/carts`;
+            axios.delete(api)
+            .then(res=>{
+                console.log('清空購物車',res);
+                this.getCart();
+            })
+            .catch(err=>{
+                console.log(err);
+            })
+        },
+        deleteCart(item){
+            this.loadingStatus.loadingItem = 'delete';
+            const api = `${url}/api/${path}/cart/${item.id}`;
+            axios.delete(api)
+            .then(res=>{
+                console.log('刪除購物車',res);
+                this.getCart();
             })
             .catch(err=>{
                 console.log(err);
@@ -91,4 +135,25 @@ const app = Vue.createApp(
 );
 
 app.component('userProductModal' , productModal);
+
+
+
+app.component('VForm', VeeValidate.Form);
+app.component('VField', VeeValidate.Field);
+app.component('ErrorMessage', VeeValidate.ErrorMessage);
+
+VeeValidate.defineRule('email', VeeValidateRules['email']);
+VeeValidate.defineRule('required', VeeValidateRules['required']);
+
+Object.keys(VeeValidateRules).forEach(rule => {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+});
+
+VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
+
+// Activate the locale
+VeeValidate.configure({
+    generateMessage: VeeValidateI18n.localize('zh_TW'),
+    validateOnInput: true, // 調整為輸入字元立即進行驗證
+});
 app.mount('#app');
