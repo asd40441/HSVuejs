424ebd95513200cde868f6504f0bfd93ac1af3a0
asd40441 <asd40441@hotmail.com>
2021-06-24 01:31:32 +0800

    updata

--- FifthWeek/all.js
+++ FifthWeek/all.js
@@ -18,10 +18,10 @@ const app = Vue.createApp(
                 // 表單結構
                 form: {
                     user: {
-                    name: '',
-                    email: '',
-                    tel: '',
-                    address: '',
+                        name: '',
+                        email: '',
+                        tel: '',
+                        address: '',
                     },
                     message: '',
                 },
@@ -134,26 +134,24 @@ const app = Vue.createApp(
 }
 );
 
-app.component('userProductModal' , productModal);
 
 
+Object.keys(VeeValidateRules).forEach(rule => {
+    if (rule !== 'default') {
+        VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+    }
+});
 
 app.component('VForm', VeeValidate.Form);
 app.component('VField', VeeValidate.Field);
 app.component('ErrorMessage', VeeValidate.ErrorMessage);
 
-VeeValidate.defineRule('email', VeeValidateRules['email']);
-VeeValidate.defineRule('required', VeeValidateRules['required']);
-
-Object.keys(VeeValidateRules).forEach(rule => {
-    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
-});
-
 VeeValidateI18n.loadLocaleFromURL('./zh_TW.json');
-
 // Activate the locale
 VeeValidate.configure({
     generateMessage: VeeValidateI18n.localize('zh_TW'),
     validateOnInput: true, // 調整為輸入字元立即進行驗證
 });
+
+app.component('userProductModal' , productModal);
 app.mount('#app');
