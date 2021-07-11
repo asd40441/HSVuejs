694b4f46f2efe33ce77e27f7fea0eb9cc7dcb1be
asd40441 <asd40441@hotmail.com>
2021-07-11 19:02:56 +0800

    updata

---
+++ SixthWeek/src/router/index.js
@@ -0,0 +1,50 @@
+import { createRouter, createWebHashHistory } from 'vue-router';
+import Home from '../views/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/products',
+    component: () => import('../views/Products.vue'),
+  },
+  {
+    path: '/cart',
+    component: () => import('../views/Cart.vue'),
+  },
+  {
+    path: '/product/:id',
+    component: () => import('../views/Product.vue'),
+  },
+  {
+    path: '/login',
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/admin',
+    component: () => import('../views/Dashboard.vue'),
+    children: [
+      {
+        path: 'products',
+        component: () => import('../views/DashboardProducts.vue'),
+      },
+      {
+        path: 'orders',
+        component: () => import('../views/DashboardOrders.vue'),
+      },
+    ],
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('../views/NotFound.vue'),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes,
+});
+
+export default router;
---
+++ SixthWeek/src/views/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>第六週 - Vue Cli、Vue Router</h1>
+</template>
